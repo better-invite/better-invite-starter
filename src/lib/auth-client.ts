f94a8be28d214e639e0cd13401b61945d45bdcaf
0-Sandy <github@sandy00.aleeas.com>
2026-03-27 19:28:16 -0300

    chore(demo): fix demo error in auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,7 @@
 import { adminClient } from "better-auth/client/plugins";
 import { twoFactor } from "better-auth/plugins";
 import { createAuthClient } from "better-auth/react";
-import { inviteClient } from "better-invite";
 
 export const authClient = createAuthClient({
-	plugins: [twoFactor(), adminClient(), inviteClient()],
+	plugins: [twoFactor(), adminClient()],
 });
